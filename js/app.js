ea0a8c7185bd2f874b8272b5242f4142cf88eb78
Mahmuda Akter <mahmuda.akter@brainstation-23.com>
2023-07-27 18:20:55 +0600

    overall reg perf component completed

--- js/app.js
+++ js/app.js
@@ -1,81 +1,7 @@
 var get_events_where_hcp_registered_url =
   "https://connect-dev.bscdp.com/api/services/clients/BRAINSTATION/veevacrm/dashboard/eventrix/hcp/registered-events?";
 
-// var overall_reg_performence_url = "https://connect-dev.bscdp.com/api/services/clients/BRAINSTATION/veevacrm/dashboard/eventrix/1000/overall-reg-performance?country_iso=DE&event_id=${output}"
-
-// ds.getDataForCurrentObject('Account', 'Id').then(
-//     function(resp) {
-//         console.log(resp);
-//         mainFunc(resp);
-//     },
-//     function(err) {
-//         console.log(err);
-//     }
-// );
-
-// function mainFunc(jsonObj) {
-//   let userId = jsonObj.Account.Id ;
-//   var url = get_events_where_hcp_registered_url + $.param({account_id: userId})
-
-//   fetch("https://connect-dev.bscdp.com/api/core/service-accounts/token", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     'cf-access-client-id': '49ba3bf0f63999679d59483cc42ad23c.access',
-//     'cf-access-client-secret': 'f6b4480418755f91ed709f51f94722066fa28645389582a990041c85b544b659'
-//   },
-//   body: JSON.stringify({
-//     client_id: "10001",
-//     client_secret: "7e6877395e9613dc0aa0a6cf6a32bc5a",
-//   }),
-// })
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Authorization failed");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     // console.log(data)
-//     const token = data.access_token;
-
-//     fetch(url, {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//         "Content-Type": "application/json",
-//         'cf-access-client-id': '49ba3bf0f63999679d59483cc42ad23c.access',
-//         'cf-access-client-secret': 'f6b4480418755f91ed709f51f94722066fa28645389582a990041c85b544b659'
-//       },
-//     }
-//   )
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Authorization failed");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       // console.log(data)
-//       const dropdown = document.getElementById('event-dropdown');
-//       dropdown.innerHTML = userId;
-//       for(let i=0 ; i<data.length; i++){
-//         const option = document.createElement('option');
-//         option.values = data[i].event_id
-//         option.text = data[i].event_title
-//         dropdown.appendChild(option);
-//         overallRegistrationPerformence(option.values)
-//       }
-//     })
-//     .catch((error) => console.log(error));
-//   })
-//   .catch((error) => console.log(error));
-// }
-
-// function overallRegistrationPerformence(eventId){
-
-// }
-
-// mainFunc();
+let colors = ['bg-primary', 'bg-info', 'bg-danger', ]
 
 fetch("https://connect-dev.bscdp.com/api/core/service-accounts/token", {
   method: "POST",
@@ -134,13 +60,6 @@ fetch("https://connect-dev.bscdp.com/api/core/service-accounts/token", {
   })
   .catch((error) => console.log(error));
 
-// function overallRegistrationPerformence(eventId){
-//   console.log(eventId)
-//   const overallRegPer = document.getElementById('overall-registration-performence');
-//   const list = document.createElement("li")
-//   list.innerHTML = eventId;
-//   overallRegPer.appendChild(list)
-// }
 
 function getOption() {
   selectElement = document.querySelector("#event-dropdown");
@@ -192,7 +111,9 @@ function getOption() {
             "#overall-registration-performence"
           );
           territories.innerHTML = ''
+          let j = 1 ;
           for (let i = 0; i < data.length; i++) {
+            
             const territory_list = document.createElement("li");
             territory_list.innerHTML = `<div class="d-flex justify-content-between">
               <h6>${data[i].territory_name}</h6>
@@ -200,7 +121,7 @@ function getOption() {
                 <span id="total_registrations_north">${data[i].hcp_in_territory_count}</span>
                 <span class="text-muted font-weight-normal"
                   >(<span id="total_registrations_north_occupancy"
-                    >${data[i].reg_percentage}</span
+                    >${data[i].reg_percentage}%</span
                   >)</span
                 >
               </p>
@@ -210,90 +131,19 @@ function getOption() {
                             aria-valuemax="100"
                             aria-valuemin="0"
                             aria-valuenow="60"
-                            class="progress-bar wd-60p bg-primary"
+                            class="progress-bar ${colors[j]}"
                             role="progressbar"
+                            style="width: ${data[i].reg_percentage}%;"
                           ></div>
                         </div>`;
             territories.appendChild(territory_list)
+            if(j === colors.length){
+              j=0;
+            }
+            j++;
           }
         })
         .catch((error) => console.log(error));
     })
     .catch((error) => console.log(error));
 }
-
-// fetch("https://bqm-demo.viquia.com/api/core/service-accounts/token", {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   body: JSON.stringify({
-//     client_id: "10001",
-//     client_secret: "7e6877395e9613dc0aa0a6cf6a32bc5a",
-//   }),
-// })
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error("Authorization failed");
-//     }
-//     return response.json();
-//   })
-//   .then((data) => {
-//     const token = data.access_token;
-//     // Make a GET request to a protected endpoint using the access token for authorization
-//     fetch(
-//       "https://bqm-demo.viquia.com/api/services/veevacrm/dashboard/:id/eventrix/real-time-users",
-//       {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       }
-//     )
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error("Authorization failed");
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         document.querySelector(
-//           "#total_registrations_in_target_area"
-//         ).innerText = data.total_registrations_in_target_area;
-//         document.querySelector(
-//           "#total_HCP_registrations_in_target_area"
-//         ).innerText = data.total_HCP_registrations_in_target_area;
-//         document.querySelector("#acheived_business_goals").innerText =
-//           data.acheived_business_goals;
-//       })
-//       .catch((error) => console.log(error));
-
-//     fetch(
-//       "https://bqm-demo.viquia.com/api/services/veevacrm/dashboard/:id/eventrix/reg-performance-overall",
-//       {
-//         headers: {
-//           Authorization: `Bearer ${token}`,
-//         },
-//       }
-//     )
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error("Authorization failed");
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         document.querySelector("#total_registrations_north").innerText = data.total_registrations_north;
-//         document.querySelector("#total_registrations_north_occupancy").innerText = data.total_registrations_north_occupancy;
-
-//         document.querySelector("#total_registrations_west").innerText = data.total_registrations_west;
-//         document.querySelector("#total_registrations_west_occupancy").innerText = data.total_registrations_west_occupancy;
-
-//         document.querySelector("#total_registrations_south").innerText = data.total_registrations_south;
-//         document.querySelector("#total_registrations_south_occupancy").innerText = data.total_registrations_south_occupancy;
-
-//         document.querySelector("#total_registrations_east").innerText = data.total_registrations_east;
-//         document.querySelector("#total_registrations_east_occupancy").innerText = data.total_registrations_east_occupancy;
-//       })
-//       .catch((error) => console.log(error));
-//   })
-//   .catch((error) => console.log(error));
--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var get_events_where_hcp_registered_url =
   "https://connect-dev.bscdp.com/api/services/clients/BRAINSTATION/veevacrm/dashboard/eventrix/hcp/registered-events?";
 
-// Getting the SFCAcID of the user  
+let colors = ["bg-primary", "bg-info", "bg-danger"];
+// Getting the SFCAcID of the user
 ds.getDataForCurrentObject("Account", "Id").then(
   function (resp) {
     console.log(resp);
@@ -14,8 +15,8 @@ ds.getDataForCurrentObject("Account", "Id").then(
 
 function mainFunc(jsonObj) {
   let userId = jsonObj.Account.Id;
-  var url =
-    get_events_where_hcp_registered_url + $.param({ account_id: userId });
+  var url = `https://connect-dev.bscdp.com/api/services/clients/BRAINSTATION/veevacrm/dashboard/eventrix/hcp/registered-events?account_id=${userId}`;
+  // get_events_where_hcp_registered_url + $.param({ account_id: userId });
 
   fetch("https://connect-dev.bscdp.com/api/core/service-accounts/token", {
     method: "POST",
@@ -57,10 +58,9 @@ function mainFunc(jsonObj) {
         })
         .then((data) => {
           const dropdown = document.getElementById("event-dropdown");
-          dropdown.innerHTML = userId;
           for (let i = 0; i < data.length; i++) {
             const option = document.createElement("option");
-            option.values = data[i].event_id;
+            option.value = data[i].event_id;
             option.text = data[i].event_title;
             dropdown.appendChild(option);
           }
@@ -70,9 +70,90 @@ function mainFunc(jsonObj) {
     .catch((error) => console.log(error));
 }
 
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   start();
-// });
+function getOption() {
+  selectElement = document.querySelector("#event-dropdown");
+  output = selectElement.value;
+  // document.querySelector('#overall-registration-performence').textContent = output;
+  fetch("https://connect-dev.bscdp.com/api/core/service-accounts/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "cf-access-client-id": "49ba3bf0f63999679d59483cc42ad23c.access",
+      "cf-access-client-secret":
+        "f6b4480418755f91ed709f51f94722066fa28645389582a990041c85b544b659",
+    },
+    body: JSON.stringify({
+      client_id: "10001",
+      client_secret: "7e6877395e9613dc0aa0a6cf6a32bc5a",
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Authorization failed");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const token = data.access_token;
+      fetch(
+        `https://connect-dev.bscdp.com/api/services/clients/BRAINSTATION/veevacrm/dashboard/eventrix/1000/overall-reg-performance?country_iso=DE&event_id=${output}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+            "cf-access-client-id": "49ba3bf0f63999679d59483cc42ad23c.access",
+            "cf-access-client-secret":
+              "f6b4480418755f91ed709f51f94722066fa28645389582a990041c85b544b659",
+          },
+        }
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Authorization failed");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const territories = document.querySelector(
+            "#overall-registration-performence"
+          );
+          territories.innerHTML = "";
+          let j = 1 ;
+          for (let i = 0; i < data.length; i++) {
+            const territory_list = document.createElement("li");
+            territory_list.innerHTML = `<div class="d-flex justify-content-between">
+              <h6>${data[i].territory_name}</h6>
+              <p class="font-weight-semibold mb-2">
+                <span id="total_registrations_north">${data[i].hcp_in_territory_count}</span>
+                <span class="text-muted font-weight-normal"
+                  >(<span id="total_registrations_north_occupancy"
+                    >${data[i].reg_percentage}%</span
+                  >)</span
+                >
+              </p>
+            </div>
+            <div class="progress ht-5">
+                          <div
+                            aria-valuemax="100"
+                            aria-valuemin="0"
+                            aria-valuenow="60"
+                            class="progress-bar ${colors[j]}"
+                            role="progressbar"
+                            style="width: ${data[i].reg_percentage}%;"
+                          ></div>
+                        </div>`;
+            territories.appendChild(territory_list);
+            if(j === colors.length){
+              j=0;
+            }
+            j++;
+          }
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
+}
 
-// queryCRMData();
 mainFunc();
